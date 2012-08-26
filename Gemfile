f372efe76a8c0a36dfbb62c63c027459730b55de
Michael Guymon <michael@tobedevoured.com>
2012-08-26 01:48:27 -0400

    use latest lockjar to try and populate the classpath

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+
+gem "lock_jar", "~> 0.5.2"
+
+group :development do
+  gem "rspec", "~> 2.11.0"
+  gem "bundler", "~> 1.1.4"
+end
--- src/main/ruby/solr_sail.rb
+++ src/main/ruby/solr_sail.rb
@@ -1,5 +1,21 @@
+require 'rubygems'
 require 'lock_jar'
+require 'java'
 
 class SolrSail
 
+  # Find lockjar relative to the gem install
+  def default_lockfile
+    File.expand_path("#{File.dirname(__FILE__)}/Jarfile.lock")
+  end
+  
+  def setup( opts )
+    lockfile = opts.delete(:lockfile) || default_lockfile
+    LockJar.install( lockfile, opts )
+    LockJar.load( lockfile, opts )
+    
+    solr = com.tobedevoured.solrsail.Solr.new
+    solr.createSolrHomeDir()
+    solr.installConfig()
+  end
 end
