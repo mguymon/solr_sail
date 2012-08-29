9f9b5dfc7fb12fc655d2c9a1bc9164b0296cb637
Michael Guymon <michael@tobedevoured.com>
2012-08-29 04:35:02 -0400

    hooray, jetty starts

--- src/main/ruby/solr_sail.rb
+++ src/main/ruby/solr_sail.rb
@@ -21,15 +21,30 @@ class SolrSail
   
   def install_config( opts )
     opts = { :jar => default_jar, :lockfile => default_lockfile }.merge( opts )
-    
     lockfile = opts.delete(:lockfile)
     jar = opts.delete(:jar)
     
+    # XXX: should use LockJar to prevent duplicates in the classpath
     $CLASSPATH << File.expand_path( jar )
+    
     LockJar.install( lockfile, opts )
     LockJar.load( lockfile, opts )
     
-    solr = com.tobedevoured.solrsail.SolrConfig.new
-    solr.install()
+    solr_config = com.tobedevoured.solrsail.SolrConfig.new
+    solr_config.install()
+  end
+  
+  def run( opts )
+    opts = { :jar => default_jar, :lockfile => default_lockfile }.merge( opts )
+    lockfile = opts.delete(:lockfile)
+    jar = opts.delete(:jar)
+        
+    # XXX: should use LockJar to prevent duplicates in the classpath
+    $CLASSPATH << File.expand_path( jar )
+    
+    LockJar.load( lockfile, opts )
+    
+    jetty = com.tobedevoured.solrsail.JettyServer.new
+    jetty.run
   end
 end
