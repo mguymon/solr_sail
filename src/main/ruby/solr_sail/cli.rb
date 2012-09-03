2e72ee8aea828d720de56620f19b4bb38d5fe916
mguymon <mguymon@tobdevoured.com>
2012-09-03 01:23:43 -0400

    update params

--- src/main/ruby/solr_sail/cli.rb
+++ src/main/ruby/solr_sail/cli.rb
@@ -5,15 +5,15 @@ require 'solr_sail'
 module SolrSail
   class CLI < Thor
     desc "install", "Install Solr config"
-    method_option :dest,  :aliases => "-d", :default => 'solr', :desc => "Path to install solr config"
+    method_option :solr_home,  :aliases => "-h", :default => 'solr', :desc => "Path to install solr config"
     def install
-        SolrSail.install_config( :dest => options[:dest] )      
+        SolrSail.install_config( :solr_home => options[:solr_home] )      
     end
     
     desc "start", "Run Solr"
-    method_option :dest,  :aliases => "-d", :default => 'solr', :desc => "Path to install solr config"
+    method_option :solr_home,  :aliases => "-h", :default => 'solr', :desc => "Path to install solr config"
     def start
-        SolrSail.start( :dest => options[:dest] )   
+        SolrSail.start( :solr_home => options[:solr_home] )   
     end
     
     desc "version", "Version"
