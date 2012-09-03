0fcda2077a3763b2f21b057b9384d6dd47af9095
mguymon <mguymon@tobdevoured.com>
2012-09-03 02:52:19 -0400

    fix relative path of version to gem install dir

--- src/main/ruby/solr_sail/version.rb
+++ src/main/ruby/solr_sail/version.rb
@@ -1,8 +1,8 @@
 module SolrSail
     
-  # Load VERSION file to VERSION var
-  if File.exists?( File.expand_path("#{File.dirname(__FILE__)}/../VERSION") )
-    VERSION = IO.read(File.expand_path("#{File.dirname(__FILE__)}/../VERSION")).strip
+  # Load VERSION file from gem to VERSION var
+  if File.exists?( File.expand_path("#{File.dirname(__FILE__)}/../../VERSION") )
+    VERSION = IO.read(File.expand_path("#{File.dirname(__FILE__)}/../../VERSION")).strip
       
   # VERSION file not found in gem dir, assume running from checkout
   else
