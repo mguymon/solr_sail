1991c37a84ce3c83490465608eedd1c62c0b790b
mguymon <mguymon@tobdevoured.com>
2012-09-03 02:46:43 -0400

    check for version

--- src/main/ruby/solr_sail/version.rb
+++ src/main/ruby/solr_sail/version.rb
@@ -1,3 +1,11 @@
 module SolrSail
-  VERSION = IO.read("VERSION").strip
+    
+  # Load VERSION file to VERSION var
+  if File.exists?( File.expand_path("#{File.dirname(__FILE__)}/../VERSION") )
+    VERSION = IO.read(File.expand_path("#{File.dirname(__FILE__)}/../VERSION")).strip
+      
+  # VERSION file not found in gem dir, assume running from checkout
+  else
+    VERSION = IO.read(File.expand_path("VERSION")).strip
+  end
 end
